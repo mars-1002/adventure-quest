48408f4ce16b36d2c8d339115392db4483663d2c
Matthew Absalon <matthewabsalonramirez@gmail.com>
2023-03-31 23:22:08 -0400

    init constants and variables

--- js/app.js
+++ js/app.js
@@ -1,4 +1,17 @@
-import player from "./playerData.js"
-console.log("Sanity check")
+/*------- Constants -------*/
+//winning condition is to clear all 3 stages
+const winCondition = 3;
 
-console.log(player)
+/*------- Variables (state) -------*/
+//dynamic variables to account for player stats, enemies imported as well as stage and whether win con is met
+import player from "./playerData.js"
+import enemy from "./enemyData.js"
+var stage, clearedStages;
+
+/*------- Cached Element References -------*/
+
+
+/*------- Event Listeners -------*/
+
+
+/*------- Functions -------*/
